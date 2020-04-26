45295e0462f67000e5b7ca9703e99800dcfb8041
Xiaoyu-freshman <59091572+Xiaoyu-freshman@users.noreply.github.com>
2020-04-26 10:26:26 +0200

    Update Data_augmentation_policy.py

--- ssd/data/transforms/Data_augmentation_policy.py
+++ ssd/data/transforms/Data_augmentation_policy.py
@@ -35,21 +35,34 @@ class  DataAaugmentationPolicy(object):
         #Color_Level Change
         if self.cfg.DATA_LOADER.AUGMENTATION_WEATHER:
             trans_color_level = A.Compose([
-                A.Cutout(num_holes=20, max_h_size=64, max_w_size=64, fill_value=255, always_apply=False, p=0.5),
+                A.Cutout(num_holes=20, max_h_size=64, max_w_size=64, fill_value=255, always_apply=False, p=0.8),
                 A.Equalize(p=1),
-                A.HueSaturationValue(hue_shift_limit=50, sat_shift_limit=50, val_shift_limit=50, always_apply=False, p=0.5),
+                A.HueSaturationValue(hue_shift_limit=50, sat_shift_limit=50, val_shift_limit=50, always_apply=False, p=0.8),
                 A.OneOf([
                     A.RandomFog(fog_coef_lower=0.3, fog_coef_upper=0.7, alpha_coef=0.08, always_apply=False, p=0.5),
                     A.RandomSnow(snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=2.5, always_apply=False, p=0.5),
                     A.RandomSunFlare(flare_roi=(0, 0, 1, 0.5), angle_lower=0, angle_upper=1, num_flare_circles_lower=6, num_flare_circles_upper=10, src_radius=400, src_color=(255, 255, 255), always_apply=False, p=0.5),
                     A.RandomRain(slant_lower=-10, slant_upper=10, drop_length=20, drop_width=1, drop_color=(200, 200, 200), blur_value=7, brightness_coefficient=0.7, rain_type=None, always_apply=False, p=0.5)
                 ]),
+                A.OneOf([
+                    A.RandomSizedBBoxSafeCrop(720, 960, erosion_rate=0.0, interpolation=1, always_apply=False, p=0.5),
+                    A.RandomSizedBBoxSafeCrop(480, 640, erosion_rate=0.0, interpolation=1, always_apply=False, p=0.5),
+                    A.RandomSizedBBoxSafeCrop(240, 320, erosion_rate=0.0, interpolation=1, always_apply=False, p=0.5),
+                ]),
+                #A.Resize(240, 320, interpolation=1, always_apply=False, p=1)
             ])
         else:
             trans_color_level = A.Compose([
                 A.Cutout(num_holes=20, max_h_size=64, max_w_size=64, fill_value=255, always_apply=False, p=0.5),
                 A.Equalize(p=1),
                 A.HueSaturationValue(hue_shift_limit=50, sat_shift_limit=50, val_shift_limit=50, always_apply=False, p=0.5),
+                A.OneOf([
+                    A.RandomSizedBBoxSafeCrop(720, 960, erosion_rate=0.0, interpolation=1, always_apply=False, p=0.5),
+                    A.RandomSizedBBoxSafeCrop(480, 640, erosion_rate=0.0, interpolation=1, always_apply=False, p=0.5),
+                    A.RandomSizedBBoxSafeCrop(240, 320, erosion_rate=0.0, interpolation=1, always_apply=False, p=0.5),
+                ]),
+                #A.Resize(240, 320, interpolation=1, always_apply=False, p=1)
+                
             ])
         #Spatial_Level
         if self.cfg.DATA_LOADER.AUGMENTATION_SPATIAL_LEVEL:
@@ -70,7 +83,7 @@ class  DataAaugmentationPolicy(object):
         bbox = np.array(bbox)
         label=augmented['category_id']
         #try rotate
-        if cfg.DATA_LOADER.AUGMENTATION_SPATIAL_LEVEL:
+        if self.cfg.DATA_LOADER.AUGMENTATION_SPATIAL_LEVEL:
             aug1 = get_aug(trans_rotate_level)
             augmented1 = aug1(**augmented)
             img1=augmented1['image']
